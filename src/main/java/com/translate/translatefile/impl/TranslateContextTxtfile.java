4a0edc771c768e3f356da1be12c4cb11165590e3
lieyanshu94 <939864132@qq.com>
2019-10-18 17:31:08 +0800

    2019年10月18日

--- src/main/java/com/translate/translatefile/impl/TranslateContextTxtfile.java
+++ src/main/java/com/translate/translatefile/impl/TranslateContextTxtfile.java
@@ -3,33 +3,57 @@ package com.translate.translatefile.impl;
 import com.translate.translatefile.TranslateContext;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TranslateContextTxtfile implements TranslateContext {
     private File trafile;
     public File translateFile(File file) {
         TranslateContext context = new TranslateContextTxtfile();
         trafile = file;
+        Map<Integer , String> map = new ConcurrentHashMap<Integer, String>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(trafile));
             String read = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            Integer num = 0;
             while ((read = br.readLine()) != null) {
-                System.out.println(read);
+                byte[] bytes = read.getBytes();
+                if (stringBuilder.toString().getBytes("utf-8").length + read.getBytes("utf-8").length < 200) {
+                    stringBuilder.append(read+"\r\n");
+                } else {
+                    map.put(num,translateSentence(stringBuilder.toString()));
+                    num++;
+                    stringBuilder.setLength(0);
+                    stringBuilder.append(read+"\r\n");
+                }
             }
+            map.put(num,translateSentence(stringBuilder.toString()));
+            for (Map.Entry<Integer,String> entry : map.entrySet()) {
+                int a = entry.getKey();
+                System.out.print(entry.getValue());
+            }
+            StringBuilder sb  = new StringBuilder();
+            for (int i = 0 ; i < map.size() ; i++) {
+                sb.append(map.get(i));
+            }
+            System.out.println(sb.toString());
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
-
         StringBuilder sb = new StringBuilder();
         context.translateSentence(sb.toString());
         return null;
     }
 
     public String translateSentence(String text) {
-        return null;
+        return text;
     }
 
     public String translateParagraph(String text) {
-        return null;
+        return text;
     }
 }
